6445906ffa1b670cfa106af39dce6441cceb0bb1
Tania <nadsanp@gmail.com>
2024-10-14 22:41:41 +0200

    modified main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 'use strict';
+//añadir la clase data con JS//
+/*
+ - seleccionar el elemento html que quiero modificar
+ - Añado la clase a ese elemento que acabo de seleccionar
+ */
+
+const list = document.querySelector('class');
+
+/*agregar el código del li desde HTML
+repetir este proceso por cada gatito*/
+
