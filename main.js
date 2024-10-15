9f25e564857800f0c025eeba92e17b1ed292e9b8
Tania <nadsanp@gmail.com>
2024-10-15 16:42:25 +0200

    create class list

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 'use strict';
+// añadir la clase js-list 
 
+const listOfCats = document.querySelector('.js-list')
+
+/* Agregar el código del li desde HTMl 
+Repetir este proceso por cada gatito */
+
+    
 
 
 
@@ -7,10 +14,4 @@
 //cuando la usuaria haga click en el boton desplegar menú
 //tenemos que darle un evento al LI para que cuando la usuaria haga click se esconda el formulario (quitar collapsed)
 
-const item=document.querySelector(".js-btn-add");
-
-
-
-
-
-
+const item=document.querySelector(".js-btn-add")
